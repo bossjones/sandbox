1bca26cf2f57543a533217fa1c9bb36a97fcd4f5
Malcolm Jones <bossjones@theblacktonystark.com>
2022-03-09 19:49:29 -0500

    woot: got it to work with tesseract, now need to clup results into a result set

--- aiopillow/aiopillow/aiotesseract.py
+++ aiopillow/aiopillow/aiotesseract.py
@@ -43,11 +43,12 @@ import aiopath
 from scenedetect import VideoManager
 from scenedetect import SceneManager
 from scenedetect.scene_manager import save_images
+from aiopillow.utils import file_functions
 
 # For content-aware scene detection:
 from scenedetect.detectors import ContentDetector
 
-VIDEO_TEST = "/Users/malcolm/dev/bossjones/sandbox/aiopillow/tests/fixtures/2615941409654231233_2615928388126090410.mp4"
+VIDEO_TEST_DIR = "/Users/malcolm/dev/bossjones/sandbox/aiopillow/processed"
 
 LOGGER = get_logger(__name__, provider="MLModels", level=logging.DEBUG)
 
@@ -141,18 +142,16 @@ async def aio_shell_runner(CMD: str, uri: str, cmd_name: str = "screendetect"):
                     LOGGER.error("exc_value: {}".format(exc_value))
                     traceback.print_tb(exc_traceback)
 
-def classify_text_in_image():
-    words_in_image_str = pytesseract.image_to_string(Image.open(VIDEO_TEST))
-    words = words_in_image_str.split("\n")
-    return words
-
 
 def execute_classify_text_in_image(ind: int, path_to_file: str):
     print(f"Thread {threading.current_thread().name}: Starting task: {ind}...")
 
     # time.sleep(1)
-    words_in_image_str = pytesseract.image_to_string(Image.open(VIDEO_TEST))
+    image = Image.open(path_to_file)
+    (width, height) = image.size  # im.size returns (width,height) tuple
+    words_in_image_str = pytesseract.image_to_string(image)
     words = words_in_image_str.split("\n")
+    rich.print(words)
 
     # if ind == 2 and RAISE_EXCEPTION:
     #     print("BOOM!")
@@ -161,80 +160,6 @@ def execute_classify_text_in_image(ind: int, path_to_file: str):
     print(f"Thread {threading.current_thread().name}: Finished task {ind}!")
     return words
 
-def copy_to_tempdir(files=None, **kw):
-    # SOURCE: https://gist.github.com/waylan/50a067d79386aabf9073dbab7beb2950
-    """
-    A decorator for building a temporary directory with prepopulated files.
-    The temproary directory and files are created just before the wrapped function is called and are destroyed
-    imediately after the wrapped function returns.
-    The `files` keyword should be a dict of file paths as keys and strings of file content as values.
-    If `files` is a list, then each item is assumed to be a path of an empty file. All other
-    keywords are passed to `tempfile.TemporaryDirectory` to create the parent directory.
-    In the following example, two files are created in the temporary directory and then are destroyed when
-    the function exits:
-        @tempdir(files={
-            'foo.txt': 'foo content',
-            'bar.txt': 'bar content'
-        })
-        def example(dir):
-            assert Path(dir, 'foo.txt').is_file()
-            p = Path(dir, 'bar.txt')
-            assert p.is_file()
-            assert p.read_text(encoding='utf-8') == 'bar content'
-    """
-    files = {f: '' for f in files} if isinstance(files, (list, tuple)) else files or {}
-    def decorator(fn):
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            with TemporaryDirectory(**kw) as td:
-                for path, content in files.items():
-                    Path(td, path).write_text(content, encoding='utf-8')
-                return fn(td, *args, **kwargs)
-        return wrapper
-    return decorator
-
-
-def execute_hello(ind: int):
-    print(f"Thread {threading.current_thread().name}: Starting task: {ind}...")
-
-    time.sleep(1)
-
-    if ind == 2 and RAISE_EXCEPTION:
-        print("BOOM!")
-        raise Exception("Boom!")
-
-    print(f"Thread {threading.current_thread().name}: Finished task {ind}!")
-    return ind
-
-# io bound feature, open video and save
-def execute_find_scenes(ind: int, video_path: str, threshold=30.0):
-    print(f"Thread {threading.current_thread().name}: Starting task: {ind}...")
-
-    # NOTE: Currently looping 3 times in a row
-
-    # Create our video & scene managers, then add the detector.
-    video_manager = VideoManager([video_path])
-    scene_manager = SceneManager()
-    scene_manager.add_detector(
-        ContentDetector(threshold=threshold))
-
-    # Improve processing speed by downscaling before processing.
-    video_manager.set_downscale_factor()
-
-    # Start the video manager and perform the scene detection.
-    video_manager.start()
-    scene_manager.detect_scenes(frame_source=video_manager)
-
-    scenes = scene_manager.get_scene_list()
-
-    res = save_images(scenes, video_manager, num_images=3, output_dir="./processed")
-
-    rich.print("===res===")
-    rich.print(res)
-
-    # Each returned scene is a tuple of the (start, end) timecode.
-    print(f"Thread {threading.current_thread().name}: Finished task {ind}!")
-    return res
 
 def execute_with_shutdown_check(f, *args, **kwargs):
     if _SHUTDOWN:
@@ -243,33 +168,28 @@ def execute_with_shutdown_check(f, *args, **kwargs):
 
     return f(*args, **kwargs)
 
-
-async def main_wait():
-    loop = asyncio.get_running_loop()
-    tasks = range(20)
-    global _SHUTDOWN
-    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as pool:
-        futures = [loop.run_in_executor(pool, execute_hello, task) for task in tasks]
-        done, pending = await asyncio.wait(
-            futures, return_when=asyncio.FIRST_EXCEPTION, timeout=3600
-        )
-        _SHUTDOWN = True
-
-    results = [done_.result() for done_ in done]
-
-    if len(pending) > 0:
-        raise Exception(f"{len(pending)} tasks did not finish")
-
-    print(f"Finished processing, got results: {results}")
-
-
-async def main_gather(tasks=20):
+async def main_gather():
     loop = asyncio.get_running_loop()
     global _SHUTDOWN
+
+    tree_list = file_functions.tree(Path(VIDEO_TEST_DIR))
+    rich.print(tree_list)
+
+    file_to_inspect = []
+
+    for p in tree_list:
+        file_to_inspect.append(f"{p}")
+
+
+    rich.print(file_to_inspect)
+
+    files_to_classify = file_functions.filter_images(file_to_inspect)
+    # tasks = len(files_to_classify)
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as pool:
         futures = [
-            loop.run_in_executor(pool, execute_with_shutdown_check, execute_find_scenes, task, VIDEO_TEST)
-            for task in range(tasks)
+            loop.run_in_executor(pool, execute_with_shutdown_check, execute_classify_text_in_image, task, path_to_img)
+            for task, path_to_img in enumerate(files_to_classify)
         ]
         try:
             results = await asyncio.gather(*futures, return_exceptions=False)
@@ -277,9 +197,9 @@ async def main_gather(tasks=20):
             print("Caught exception", ex)
             # _SHUTDOWN = True
             raise
-    print(f"Finished processing, got results: {results}")
+    rich.print(f"Finished processing, got results: {results}")
 
 
 # asyncio.run(main_wait())
 if __name__ == "__main__":
-    asyncio.run(main_gather(tasks=10))
+    asyncio.run(main_gather())
